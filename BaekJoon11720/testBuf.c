c8f21f0026d184579894b28c5157504ec37cb3c6
chycs7747 <chycs98@naver.com>
2022-11-12 14:52:26 +0900

    %d to interpret %c (lick ascii number 10-LF)

--- BaekJoon11720/testBuf.c
+++ BaekJoon11720/testBuf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 int main(void) {
-    char a='\0';
+    char a='1';
     int count=0;
     while(a != '\n') {
         scanf("%c", &a);
+        printf("%d\n", a); // %c로 출력안되는 것을 %d로 출력함으로써 확인 가능하다!!
         count++; 
     }
     printf("%d", count); // strlen(a) + 1
