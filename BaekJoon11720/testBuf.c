73bd66819287e6def5de5f8ed0f6af83ab738f6b
chycs7747 <chycs98@naver.com>
2022-11-12 14:55:33 +0900

    Clean up the buffer concept of "%c"

--- BaekJoon11720/testBuf.c
+++ BaekJoon11720/testBuf.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 int main(void) {
-    char a='1';
+    char a='\0';
     int count=0;
     while(a != '\n') {
-        scanf("%c", &a);
+        scanf("%c", &a); // 버퍼에 문자가 없으면 하나 입력받고 a에 저장, 이미 있으면 버퍼에서 맨 앞꺼 하나 빼서 a에 저장.
         printf("%d\n", a); // %c로 출력안되는 것을 %d로 출력함으로써 확인 가능하다!!
-        count++; 
-    }
-    printf("%d", count); // strlen(a) + 1
-    while(a != '\n') {
-        printf("%c", a); //it doesn't work
+        count++; // \n까지 들어오고 조건문을 가므로 최종 count는 문자개수+1이 된다.
     }
 
     return 0;
